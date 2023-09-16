3e6a408da16de7ddb8eae12bb2aa8171fdefc54b
Artem Martiyanov <artem-martiyanov@mail.ru>
2023-09-16 10:41:57 +0300

    Init

--- src/components/layout/page-wrapper/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-
-
-export const Main = styled.main`
-  flex-grow: 1;
-  
-  color: ${({theme}) => theme};
-`
--- src/models/IUser.ts
+++
@@ -1,1 +0,0 @@
-export interface IUser {  name: string | null,  password: string | null,  email: string | null,}
